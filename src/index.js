8ee18eb87b158afbf9b98dd1a79c91c84073641e
Vladzislav Khurs <130601125+VladKhurs@users.noreply.github.com>
2023-08-25 17:47:05 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let flag = true
+  let br = ['[]', '{}', '||', '()', '12', '34', '56', '77', '88']
+  for(let k = 0; k < bracketsConfig.length * 2; k++){
+
+
+    for(let j = 0; j < bracketsConfig.length; j++){
+      while(str.indexOf(bracketsConfig[j][0] + bracketsConfig[j][1]) != -1){
+            for(let i = 1; i < str.length; i++){
+                if(str[i - 1] === bracketsConfig[j][0] && str[i] === bracketsConfig[j][1]){
+                    str = str.replace(bracketsConfig[j][0] + bracketsConfig[j][1], '')
+                }
+            }
+      }
+    }
+  }
+  if(str.length != 0){
+      flag = false
+  }
+  return flag
 }
